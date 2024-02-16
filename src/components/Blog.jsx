4b97e53485fd16e2491f52ad4e1daa84a9b746a3
samieruashovo <samieruashovo@gmail.com>
2024-02-16 17:53:34 +0600

    added cs freshmen blog

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { useState } from "react";
 import b1 from "../assets/blog/b1.png";
 import b2 from "../assets/blog/b2.png";
+import b3 from "../assets/blog/tcl.png";
 
 import About from "./About";
 import HigherStudy from "../blogs/HigherStudy";
 import ErasmusIfros from "../blogs/ErasmusIfros";
+import CareerAdv from "../blogs/CareerAdvForCs";
 
 const Blog = () => {
   const [activeComponent, setActiveComponent] = useState("");
@@ -17,6 +19,8 @@ const Blog = () => {
         return <HigherStudy />;
       case "ei":
         return <ErasmusIfros />;
+      case "cs":
+        return <CareerAdv />;
 
       default:
         return null;
@@ -31,18 +35,27 @@ const Blog = () => {
 
   const blogs = [
     {
-      title: "A Comprehensive Guide to Securing Full-Funded Scholarships for Higher Studies Abroad",
+      title:
+        "A Comprehensive Guide to Securing Full-Funded Scholarships for Higher Studies Abroad",
       desc: "Embarking on a journey toward higher studies is a dream for many individuals seeking academic excellence and personal growth. However, the financial burden associated with pursuing",
       link: "higher-studies-abroad",
       img: b1,
       sf: "hs",
     },
     {
-      title: "Navigating the Path to Erasmus Masters at IFRoS: A Personal Journey",
+      title:
+        "Navigating the Path to Erasmus Masters at IFRoS: A Personal Journey",
       desc: "In this blog post, I'll detail my journey applying for an Erasmus Masters program at IFRoS. I'll discuss the requirements, evaluation criteria, and essential steps I took to secure acceptance, offering valuable insights for prospective applicants.",
       link: "path-to-erasmus-masters",
       img: b2,
       sf: "ei",
+    },
+    {
+      title: "Beyond the Trends: Timeless Career Advice for CS Freshmen",
+      desc: "As a fellow CS student, I’ve been down that road of uncertainty and excitement. Let me share some insights on competitive programming.",
+      link: "beyond-the-trends",
+      img: b3,
+      sf: "cs",
     },
   ];
   return (
@@ -56,7 +69,10 @@ const Blog = () => {
           <div class="cards">
             {blogs.map((blog) => (
               <div class="card">
-                <a className="blog-link img-thumbd" onClick={() => handleButtonClick(blog.sf)}>
+                <a
+                  className="blog-link img-thumbd"
+                  onClick={() => handleButtonClick(blog.sf)}
+                >
                   <img src={blog.img} className="img-thumb" alt={blog.title} />
                   <div class="card-item">
                     <div class="title hover-link">{blog.title}</div>
